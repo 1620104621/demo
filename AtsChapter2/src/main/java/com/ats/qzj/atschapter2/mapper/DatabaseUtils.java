16e3676acdabb6b6c27441091dd2c4a2f459d227
qzj98 <qzj98@user.noreply.gitee.com>
2024-08-07 10:30:22 +0800

    随意修改了下

--- AtsChapter2/src/main/java/com/ats/qzj/atschapter2/mapper/DatabaseUtils.java
+++ AtsChapter2/src/main/java/com/ats/qzj/atschapter2/mapper/DatabaseUtils.java
@@ -10,5 +10,5 @@ import java.util.List;
 @Mapper
 //@Repository
 public interface DatabaseUtils {
-    List<SettleEntity> findCardDetailsByIds(@Param("id") Long id);
+    List<SettleEntity> findCardDetailsByIds( @Param("id") Long id);
 }
